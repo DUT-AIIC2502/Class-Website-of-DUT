c4841afddd75c308f09304db170353e2228c24aa
FengZhiHen <lwr0801430@163.com>
2025-10-11 20:02:46 +0800

    1. 创建了父模板，准备使用模板继承

---
+++ others/test.py
@@ -0,0 +1,7 @@
+from common.control_func import send_qq_message
+
+if __name__ == '__main__':
+    send_qq_message(
+        contact_name="李泽灏",
+        message="【自动消息】这是我通过python向你发送的第一条消息！"
+    )
