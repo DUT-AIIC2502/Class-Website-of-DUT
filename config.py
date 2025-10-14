043928111d2a03e9ae1543580efb95e8de9876e4
FengZhiHen <lwr0801430@163.com>
2025-10-14 10:09:38 +0800

    1. 添加了“生产”和“测试”两种运行环境

--- config.py
+++ config.py
@@ -20,8 +20,22 @@ class Config(object):
     SCHEDULER_EXECUTORS = {'default': {'type': 'threadpool', 'max_workers': 10}}  # 配置执行器
 
     # 其他配置
-    DEBUG = True
-    # TESTING = False
-    SESSION_KEY = '12345qazxc'
+    # DEBUG = True
+    SESSION_KEY = '123456'
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=10)
-    SCHEDULER_API_ENABLED = True
+
+
+# 开发服务器运行参数
+RUN_SETTINGS_MAIN = {
+    'host': '0.0.0.0',
+    'port': 5000,
+    'debug': False,
+    'use_reloader': False
+}
+
+RUN_SETTINGS_DEV = {
+    'host': '127.0.0.1',
+    'port': 5001,
+    'debug': True,
+    'use_reloader': False
+}
--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 """运行应用"""
 from app import create_app
+from config import RUN_SETTINGS_MAIN, RUN_SETTINGS_DEV
 
 # 创建应用
 app = create_app()
 
+# 运行环境。如果是生产环境，则设置为"main"；如果是开发环境，则设置为"dev"
+branch = 'dev'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, use_reloader=False)
+    if branch == 'main':
+        app.run(**RUN_SETTINGS_MAIN)
+    elif branch == 'dev':
+        app.run(**RUN_SETTINGS_DEV)
