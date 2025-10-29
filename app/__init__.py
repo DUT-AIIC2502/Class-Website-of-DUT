c35ef27c68d0cd40b8a2a8ca5c071a73ba211f7a
风之痕 <lwr0801430@163.com>
2025-10-29 23:23:26 +0800

    Feature/auth (#8)
    * 对auth蓝图进行了模块化拆分
    
    * 实现了main蓝图的模块化
    
    * 修复了注销账户的bug
    
    * 修改了一些注释

--- app/__init__.py
+++ app/__init__.py
@@ -91,8 +91,8 @@ def create_app():
     login_manager.init_app(app)
 
     # 导入蓝图
-    from app.blueprints.main.routes import main_bp
-    from app.blueprints.auth.routes import auth_bp
+    from app.blueprints.main import main_bp
+    from app.blueprints.auth import auth_bp
     from app.blueprints.info_management.routes import info_management_bp
     from app.blueprints.learning_space.routes import learning_space_bp
     from app.blueprints.notices import notices_bp
---
+++ app/blueprints/auth/user_management.py
@@ -0,0 +1,96 @@
+from . import auth_bp
+from .helpers import get_top_role
+
+import re
+from flask import render_template, request, redirect, url_for
+
+from models import User, Role
+from ext import db
+from decorators import role_required
+
+
+@auth_bp.route('/user_management/', methods=['GET', 'POST'])
+@role_required('User', 'Root')
+def user_management():
+    if request.method == 'GET':
+        all_users = User.query.all()
+        # 生成展示序列
+        roots_list = []
+        admins_list = []
+        users_list = []
+        guests_list = []
+        for user in all_users:
+            user_roles = [role.name for role in user.roles]
+            user_top_role = get_top_role(user_roles)
+            new_list = [user.real_name, user.student_id, user_top_role, user.status]
+            if user_top_role == 'Root':
+                roots_list.append(new_list)
+            elif user_top_role == 'Admin':
+                admins_list.append(new_list)
+            elif user_top_role == 'User':
+                users_list.append(new_list)
+            elif user_top_role == 'Guest':
+                guests_list.append(new_list)
+
+        all_list = [roots_list, admins_list, users_list, guests_list]
+
+        return render_template('auth/user_management.html', all_list=all_list)
+
+    elif request.method == 'POST':
+        form_get = request.form.to_dict()
+
+        if form_get['method'] in ('insert_user', 'delete_choose'):
+
+            if form_get['method'] == 'insert_user':
+                return redirect(url_for('auth.register'))
+
+            elif form_get['method'] == 'delete_choose':
+                pass
+
+        else:
+            """
+            处理用户信息
+            """
+            # 根据学号，查找该用户信息
+            student_id = re.findall(r'\d+', form_get['method'])
+            user = User.query.filter_by(student_id=student_id).first()
+
+            # 获取最高身份
+            user_roles = [role.name for role in user.roles]
+            user_top_role = get_top_role(user_roles)
+
+            """验证数据库中存在该用户"""
+            if not user:
+                return f"<script> alert('不存在该用户！');window.open('{url_for('auth.login')}');</script>"
+
+            """获取角色对象"""
+            if 1 == 1:
+                role_root = Role.query.filter(Role.name == 'Root').first()
+                role_admin = Role.query.filter(Role.name == 'Admin').first()
+                role_user = Role.query.filter(Role.name == 'User').first()
+                role_guest = Role.query.filter(Role.name == 'Guest').first()
+
+            if 'upgrade' in form_get['method']:
+                """角色升级"""
+                if user_top_role == 'Admin':
+                    user.add_role(role_root)
+                elif user_top_role == 'User':
+                    user.add_role(role_admin)
+                elif user_top_role == 'Guest':
+                    user.add_role(role_user)
+
+            elif 'downgrade' in form_get['method']:
+                """角色降级"""
+                if user_top_role == 'Root':
+                    user.remove_role(role_root)
+                elif user_top_role == 'Admin':
+                    user.remove_role(role_admin)
+                elif user_top_role == 'User':
+                    user.remove_role(role_user)
+
+            elif 'delete' in form_get['method']:
+                db.session.delete(user)
+
+            db.session.commit()
+
+            return redirect(url_for('auth.user_management'))
